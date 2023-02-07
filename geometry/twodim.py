31dac678ce1e510f0fb2f64a6bf02791f4965eee
Vibhav Gopal <vibhavgopal2004@gmail.com>
2023-02-07 17:08:56 +0530

    formatted files to look good

--- geometry/twodim.py
+++ geometry/twodim.py
@@ -1,25 +1,25 @@
 class IllegalPoint(Exception):
     "Invalid Point"
 
+
 class Point():
-    def __init__(self, coordinate:tuple):
+    def __init__(self, coordinate: tuple) -> None:
         # (x,y) input
-        if(len(coordinate)!=2):
-            
+        if (len(coordinate) != 2):
             raise IllegalPoint
         self.xcoord = coordinate[0]
         self.ycoord = coordinate[1]
 
-    def dist2origin(self):
+    def dist2origin(self) -> float:
         dist = (self.xcoord**2 + self.ycoord**2)**0.5
         return dist
-        
+
 
 class Line():
-    def __init__(self,equation:str) :
-        #Stored in (a,b,c) tuple, of equation ax+by+c=0
-        #Parse
-        while(equation.find(' ') != -1):
+    def __init__(self, equation: str):
+        # Stored in (a,b,c) tuple, of equation ax+by+c=0
+        # Parse
+        while (equation.find(' ') != -1):
             pos = equation.find(' ')
             equation = list(equation)
             equation.pop(pos)
@@ -28,10 +28,7 @@ class Line():
         posx = equation.find('x')
         posy = equation.find('y')
         posequal = equation.find('=')
-        
+
         self.xcoeff = float(equation[0:posx])
         self.ycoeff = float(equation[posx+1:posy])
         self.constant = float(equation[posy+1:posequal])
-        
-    
-
