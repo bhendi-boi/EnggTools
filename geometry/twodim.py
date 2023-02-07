3b7d92ecf1dd4cb2cf857d19c9e66ecf8ee72e96
Vibhav Gopal <vibhavgopal2004@gmail.com>
2023-02-07 16:44:32 +0530

    added files

---
+++ geometry/twodim.py
@@ -0,0 +1,20 @@
+class Line():
+    def __init__(self,equation:str) :
+        #Stored in (a,b,c) tuple, of equation ax+by+c=0
+        #Parse
+        while(equation.find(' ') != -1):
+            pos = equation.find(' ')
+            equation = list(equation)
+            equation.pop(pos)
+            equation = ''.join(equation)
+
+        posx = equation.find('x')
+        posy = equation.find('y')
+        posequal = equation.find('=')
+        
+        self.xcoeff = float(equation[0:posx])
+        self.ycoeff = float(equation[posx+1:posy])
+        self.constant = float(equation[posy+1:posequal])
+        
+    
+
