287f517f0a57e4cc17b59ce91bf97f148b644587
Vibhav Gopal <vibhavgopal2004@gmail.com>
2023-02-07 16:50:11 +0530

    added point class

--- geometry/twodim.py
+++ geometry/twodim.py
@@ -1,3 +1,20 @@
+class Point():
+    def __init__(self, coordinate:str):
+        # (x,y) input
+        while (coordinate.find(' ') != -1):
+            pos = coordinate.find(' ')
+            coordinate = list(coordinate)
+            coordinate.pop(pos)
+            coordinate = ''.join(coordinate)
+        poscomma = coordinate.find(',')
+        self.xcoord = float(coordinate[1:poscomma])
+        self.ycoord = float(coordinate[poscomma+1,:])
+
+    def dist2origin(self):
+        dist = (self.xcoord**2 + self.ycoord**2)**0.5
+        return dist
+        
+
 class Line():
     def __init__(self,equation:str) :
         #Stored in (a,b,c) tuple, of equation ax+by+c=0
