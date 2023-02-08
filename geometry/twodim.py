51625598f86eb6bf5d59e797b76af79008fb1b3b
Vibhav Gopal <vibhavgopal2004@gmail.com>
2023-02-08 16:10:14 +0530

    Added a new method, that takes two point objects as input and returns a line object. The method is independent from existing classes.

--- geometry/twodim.py
+++ geometry/twodim.py
@@ -3,6 +3,7 @@ try:
 except:
     pass
 
+
 class IllegalPoint(Exception):
     "Invalid Point"
 
@@ -37,30 +38,47 @@ class Point():
 
 
 class Line():
-    def __init__(self,equation:tuple) -> None:
-        #Stored in (a,b,c) tuple, of equation ax+by+c=0
-        #Parse     
+    def __init__(self, equation: tuple) -> None:
+        # Stored in (a,b,c) tuple, of equation ax+by+c=0
+        # Parse
         self.xcoeff = equation[0]
         self.ycoeff = equation[1]
         self.constant = equation[2]
-        
-        
-    def cointainsPoint(self, point:Point)->bool:
+
+
+    def containsPoint(self, point: Point) -> bool:
         xcoord = point.xcoord
         ycoord = point.ycoord
-        if int(self.xcoeff* xcoord + self.ycoeff*ycoord + self.constant):
+        if int(self.xcoeff * xcoord + self.ycoeff*ycoord + self.constant):
             return False
         return True
 
-    def onSameSide(self,p1:Point, p2:Point) -> bool:
+    def onSameSide(self, p1: Point, p2: Point) -> bool:
         e1 = self.xcoeff*p1.xcoord + self.ycoeff*p1.ycoord+self.constant
         e2 = self.xcoeff*p2.xcoord + self.ycoeff*p2.ycoord+self.constant
-        sign1 = math.copysign(1,e1)
-        sign2 = math.copysign(1,e2)
-        if sign1==sign2:
+        sign1 = math.copysign(1, e1)
+        sign2 = math.copysign(1, e2)
+        if sign1 == sign2:
             return True
         else:
             return False
-        
-    
 
+def points2line(point1:Point,point2:Point) -> Line:
+    x1,y1 = (point1.xcoord,point1.ycoord)
+    x2,y2 = (point2.xcoord,point2.ycoord)
+    
+    if x1==x2:
+        slope = 'inf'
+    else:
+        slope = (y2-y1)/(x2-x1)
+    
+    if slope == 'inf':
+        ycoeff=0
+        xcoeff=1
+        constant=(-1)*x1
+    else:
+        xcoeff = y1-y2
+        ycoeff = x2-x1
+        constant = (x1-x2)*y1+x1*(y2-y1)
+    return Line((xcoeff,ycoeff,constant))
+    
