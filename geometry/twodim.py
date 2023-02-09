e2a1dba58aa51898e3b0d9152ab9fbaaabd27571
Praneeth <tadpraneeth23@gmail.com>
2023-02-09 22:51:02 +0530

    added comment

--- geometry/twodim.py
+++ geometry/twodim.py
@@ -89,7 +89,7 @@ class Circle():
         self.area = math.pi*radius*radius
         self.diameter = self.radius*2
 
-
+# enduku ra
 class Triangle():
     def __init__(self, a: Point, b: Point, c: Point) -> None:
         self.a = a
