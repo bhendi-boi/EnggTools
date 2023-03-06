eb9c6b624ddf6e02e527d723c710ec43535069d3
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-06 21:53:18 +0530

    added __init__ method for le2Variables

--- algebra/linear/le2Varibles.py
+++ algebra/linear/le2Varibles.py
@@ -1,10 +1,12 @@
+from typing import Tuple
 
 
 class LinearEquation2Varaibles:
-    # ? add constructor and a solution when provided with another le2variables 
-    # ax + by + c = 0 
-    def __init__(self, a:float, b:float, c:float):
-        pass
+    # ax + by + c = 0
+    def __init__(self, a:float, b:float, c:float)-> None:
+        self.a = a
+        self.b = b
+        self.c = c
 
-    def solve(self, eq2:"LinearEquation2Varaibles"):
+    def solve(self, eq2:"LinearEquation2Varaibles")-> Tuple[float]:
         pass
