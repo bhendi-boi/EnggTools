59df0a843da2eda81c1d5b40a48e8ab21375f410
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-06 11:36:00 +0530

    created package for algebra created subpackage for linear algebra added todos for linear equation in 2 variables and 3 variables

---
+++ algebra/linear/le2Varibles.py
@@ -0,0 +1,10 @@
+
+
+class LinearEquation2Varaibles:
+    # ? add constructor and a solution when provided with another le2variables 
+    # ax + by + c = 0 
+    def __init__(self, a:float, b:float, c:float):
+        pass
+
+    def solve(self, eq2:"LinearEquation2Varaibles"):
+        pass
---
+++ algebra/linear/le3Variables.py
@@ -0,0 +1,10 @@
+
+
+class LinearEquation3Varaibles:
+    # ? add constructor and a solution when provided with 3 le3Variables
+    # ax + by + cz + d = 0 
+    def __init__(self, a:float, b:float,c:float, d:float)-> "LinearEquation3Varaibles":
+        pass
+
+    def solve(self, eq2:"LinearEquation3Varaibles", eq3: "LinearEquation3Varaibles"):
+        pass
