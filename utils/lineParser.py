55c459f3c07b0f2e9e40d1b9e39e4cc98f903bf2
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-07 22:53:52 +0530

    lineParser kinds works for edge case discussed above Have to test this

--- utils/lineParser.py
+++ utils/lineParser.py
@@ -1,14 +1,18 @@
 from typing import Tuple
 
-def lineParser(input:str)->Tuple[float]:
-    coordinates = [0,0,0]
+
+def lineParser(input: str) -> Tuple[float]:
+    coordinates = [0, 0, 0]
     # removing spaces
     input = input.replace(" ", "")
     for i in range(len(input)):
         if input[i].lower() == "x":
-            coordinates[0] = input[i-1]
+            coordinates[0] = float(input[i - 1])
         elif input[i].lower() == "y":
-            coordinates[1] = input[i-1]
-        elif input[i] == '=':
-            coordinates[2] = input[i-1]
+            coordinates[1] = float(input[i - 1])
+        elif input[i] == "=":
+            if input[-1] != "0":
+                coordinates[2] = float(input[-1])
+            else:
+                coordinates[2] = float(input[i - 1])
     return coordinates
