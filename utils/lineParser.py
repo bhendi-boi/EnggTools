3552682cfeee87be0c0221b3067ab28efd9dc58a
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-07 22:43:43 +0530

    lineParser works except when input is given like this 2x+3y=-2 .

--- utils/lineParser.py
+++ utils/lineParser.py
@@ -1,4 +1,14 @@
 from typing import Tuple
 
 def lineParser(input:str)->Tuple[float]:
-    pass
+    coordinates = [0,0,0]
+    # removing spaces
+    input = input.replace(" ", "")
+    for i in range(len(input)):
+        if input[i].lower() == "x":
+            coordinates[0] = input[i-1]
+        elif input[i].lower() == "y":
+            coordinates[1] = input[i-1]
+        elif input[i] == '=':
+            coordinates[2] = input[i-1]
+    return coordinates
